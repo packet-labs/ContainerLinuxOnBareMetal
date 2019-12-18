e08d5281c34160605ef906f35be1ad2d5e3b3d7e
John Studarus <john@jhlconsulting.com>
2019-12-18 23:27:26 +0000

    spins up two instances

---
+++ Workspace/instance.tf
@@ -0,0 +1,34 @@
+resource "packet_ssh_key" "ssh-key" {
+  name       = "mykey"
+  public_key = file("mykey.pub")
+}
+
+# testing with IPv6
+#resource "packet_reserved_ip_block" "elastic_ip" {
+#  project_id = "${var.packet_project_id}"
+#  type       = "public_ipv4"
+#  quantity   = 1
+#}
+
+resource "packet_device" "hosts" {
+  depends_on = [packet_ssh_key.ssh-key]
+
+  hostname = format("%s-%s-%02d", var.lab_name, var.facility, count.index)
+
+  plan             = "t1.small.x86"
+  facilities       = [var.facility]
+  operating_system = "flatcar_stable"
+  billing_cycle    = "hourly"
+  project_id       = var.packet_project_id
+  count            = var.instance_count
+
+  tags = [var.lab_name]
+
+  connection {
+    host        = self.access_public_ipv4
+    type        = "ssh"
+    user        = "root"
+    private_key = file("mykey")
+  }
+}
+
---
+++ Workspace/vars.tf
@@ -0,0 +1,24 @@
+variable "packet_auth_token" {
+}
+
+variable "packet_project_id" {
+}
+
+variable "bgp_md5" {
+}
+
+variable "lab_number" {
+}
+
+variable "lab_name" {
+}
+
+variable "facility" {
+  default = "ewr1"
+}
+
+# number of hosts to deploy
+variable "instance_count" {
+  default = "2"
+}
+
