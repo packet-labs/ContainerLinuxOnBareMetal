285a4dae4983980ad517129d6ac6277647d3365c
Maxime Guyot <maxime@root314.com>
2020-02-20 15:36:39 +0100

    Add output for multiple servers

--- Workspace/output.tf
+++ Workspace/output.tf
@@ -2,6 +2,9 @@ output "Server_IPs_v4" {
   value = packet_device.hosts.*.access_public_ipv4
 }
 
-output "SSH_Access_Server_0" {
-  value = "ssh core@${packet_device.hosts[0].access_public_ipv4} -i mykey"
+output "SSH_Access_Servers" {
+  value = {
+    for server in packet_device.hosts:
+    server.hostname => "ssh core@${server.access_public_ipv4}"
+  }
 }
